aa6a8e55b58609304f916d7d2afaaf668cb6be03
Lagain <99564089+Lagain@users.noreply.github.com>
2023-01-02 20:27:58 -0600

    Set up websites list

---
+++ server-check.py
@@ -0,0 +1,11 @@
+import requests
+
+websites = [
+    "https://www.twitter.com",
+    "https://www.reddit.com",
+    "https://store.steampowered.com"
+]
+
+statuses = {
+    200: "Website Available"
+}
